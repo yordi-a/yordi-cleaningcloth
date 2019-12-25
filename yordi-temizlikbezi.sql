0dd14b60db421df381ca44a0ef57e013ffe1cddf
Selim Günay <54241778+LuLisTaken@users.noreply.github.com>
2019-12-25 17:07:41 +0300

    Gerekli Eşyalar

--- yordi-temizlikbezi.sql
+++ yordi-temizlikbezi.sql
@@ -1,3 +1,7 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-('temizlikbezi', 'Temizlik Bezi', 5, 0, 1),
-('kirlitemizlikbezi', 'Kirli Temizlik Bezi', 5, 0, 1),
+('temizlikbezi', 'Temizlik Bezi 3/3', 5, 0, 1),
+('temizlikbezi1', 'Temizlik Bezi 2/3', 5, 0, 1),
+('temizlikbezi2', 'Temizlik Bezi 1/3', 5, 0, 1),
+('kirlitemizlikbezi', 'Kirli Temizlik Bezi 3/3', 5, 0, 1),
+('kirlitemizlikbezi1', 'Kirli Temizlik Bezi 2/3', 5, 0, 1),
+('kirlitemizlikbezi2', 'Kirli Temizlik Bezi 1/3', 5, 0, 1),
